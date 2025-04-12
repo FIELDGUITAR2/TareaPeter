bdfb2aae36d6a6d9180fd83edca76a3a1bb194b4
FILEDGUITAR2 <josagoor@gmail.com>
2025-04-12 15:07:12 -0500

    Nombres y apellidos

--- Secciones/crear_Eliminar.php
+++ Secciones/crear_Eliminar.php
@@ -2,6 +2,61 @@
     include('../Templates/cabecera.php');
 ?>
 
+<div class="col-md-5">
+    <div class="mb-3">
+        <label for="" class="form-label">Primer Nombre</label>
+        <input
+            type="text"
+            class="form-control"
+            name=""
+            id=""
+            aria-describedby="helpId"
+            placeholder=""
+        />
+        <small id="helpId" class="form-text text-muted">Inserte aqui el <strong>Primer nombre</strong></small>
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Segundo Nombre</label>
+        <input
+            type="text"
+            class="form-control"
+            name=""
+            id=""
+            aria-describedby="helpId"
+            placeholder=""
+        />
+        <small id="helpId" class="form-text text-muted">Inserte aqui el <strong>Segundo nombre</strong></small>
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Primer Apellido</label>
+        <input
+            type="text"
+            class="form-control"
+            name=""
+            id=""
+            aria-describedby="helpId"
+            placeholder=""
+        />
+        <small id="helpId" class="form-text text-muted">Inserte aqui el <strong>Primer apellido</strong></small>
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Segundo Apellido</label>
+        <input
+            type="text"
+            class="form-control"
+            name=""
+            id=""
+            aria-describedby="helpId"
+            placeholder=""
+        />
+        <small id="helpId" class="form-text text-muted">Inserte aqui el <strong>Segundo apellido</strong></small>
+    </div>
+        
+</div>
+<div class="col-md-7">
+
+</div>
+
 <?php
     include('../Templates/pie.php');
 ?>
