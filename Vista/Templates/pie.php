cd145452cfc8b33c3c02d73481a922c3d5a7ee8c
HenryArcila12 <hjarcilap@udistrital.edu.co>
2025-04-15 16:34:27 -0500

    Henry
    Vista

--- Vista/Templates/pie.php
+++ Vista/Templates/pie.php
@@ -1,4 +1,7 @@
 </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-q1TfS9+Zz9ZLhX7bR8/9Z2PLNfChFdpTmtq6FqfWgbZT4e+OxgW1IWXh9S1TBSI+"
+crossorigin="anonymous"></script>
 </body>
 </html>
--- Vista/vista_Nomina.php
+++ Vista/vista_Nomina.php
@@ -1,3 +1,37 @@
 <?php include('../Templates/cabecera.php');?>
-    Vista Nomina
+<div class="container">
+    <h2 class="text-center mt-4 mb-4">Listado de Nómina</h2>
+    
+    <table class="table table-bordered table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th>Nombre</th>
+                <th>Cargo</th>
+                <th>Salario</th>
+                <th>Días Laborados</th>
+                <th>Salud</th>
+                <th>Pensión</th>
+                <th>Total Devengado</th>
+                <th>Total Deducciones</th>
+                <th>Total a Pagar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($listaNomina as $nomina): ?>
+                <tr>
+                    <td><?= htmlspecialchars($nomina['nombre']) ?></td>
+                    <td><?= htmlspecialchars($nomina['cargo']) ?></td>
+                    <td>$<?= number_format($nomina['salario'], 0, ',', '.') ?></td>
+                    <td><?= $nomina['dias_laborados'] ?></td>
+                    <td>$<?= number_format($nomina['salud'], 0, ',', '.') ?></td>
+                    <td>$<?= number_format($nomina['pension'], 0, ',', '.') ?></td>
+                    <td>$<?= number_format($nomina['total_devengado'], 0, ',', '.') ?></td>
+                    <td>$<?= number_format($nomina['total_deducciones'], 0, ',', '.') ?></td>
+                    <td><strong>$<?= number_format($nomina['total_pagar'], 0, ',', '.') ?></strong></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
 <?php include('../Templates/pie.php');?>
